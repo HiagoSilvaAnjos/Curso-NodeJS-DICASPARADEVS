89419baa44ca96baf9e9880debc3488eb2843297
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-02-21 17:25:16 -0300

    Update express_db.js

--- modules/express_db.js
+++ modules/express_db.js
@@ -7,13 +7,61 @@ app.post('/users', async (req, res) => {
 
     try {
         const user = await UserModel.create(req.body);
-        res.status(201).json(user);
+        return res.status(201).json(user);
     } catch (error) {
-        res.status(500).send(error.message);
+        return res.status(500).send(error.message);
     }
 
 })
 
+app.get("/users", async (req, res) => {
+    try {
+        const users = await UserModel.find({})
+        return res.status(200).json(users);
+    }
+    catch (error) {
+        return res.status(500).send(error.message);
+    }
+})
+
+app.get("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+
+        return res.status(200).json(user);
+    }
+    catch (error) {
+        return res.status(500).send(error.message);
+    }
+})
+
+app.patch("/users/:id", async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+        return res.status(200).json(user);
+
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+
+})
+
+app.delete("/users/:id", async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndDelete(id);
+
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+
+})
 
 const port = 3000;
 app.listen(port, () => console.log(`listenig on port ${port}`));
